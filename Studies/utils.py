b775385fd8bf032f3e22638666d1d694deaf3195
Paul Greene <paul.greene@sedarotech.com>
2023-12-14 17:43:22 -0500

    Monte_carlo fleshed out, Need to run/create results

--- Studies/utils.py
+++ Studies/utils.py
@@ -2,9 +2,13 @@
 
 
 
+from os import path
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import flatten_json
+from fuzzywuzzy import fuzz
+
 from typing import TypeAlias
 Quaternion: TypeAlias =  np.ndarray
 
@@ -85,3 +89,50 @@ def studyStats(job_id_to_dataframe, series):
     study_roll_df = pd.DataFrame(data)
     return study_roll_df.describe()
         
+class AgentModelParametersOverridePaths():
+    def __init__(self, wildfire_scenario_branch, wildfire_agent_branch, agent_name='Wildfire'):
+        self.wildfire_scenario_branch = wildfire_scenario_branch
+        self.wildfire_agent_branch    = wildfire_agent_branch
+        self.agent_id_name_map, self.agent_name_id_map = self.create_agent_name_id_maps()
+        self.path_to_agent_key = self.create_path_to_agent_dict(agent_name)
+
+    def create_agent_name_id_maps(self): 
+        self.scenario_flat       = flatten_json.flatten( self.wildfire_scenario_branch.data, '.') 
+        agent_id_name_map   = { key.split('.')[1]: value for (key,value) in self.scenario_flat.items() if key.endswith('name') } 
+        agent_name_id_map   = { value: key.split('.')[1] for (key,value) in self.scenario_flat.items() if key.endswith('name') } 
+        return agent_id_name_map, agent_name_id_map
+
+    def create_path_to_agent_dict(self, agent_name='Wildfire'):
+        agent_flat = flatten_json.flatten( self.wildfire_agent_branch.data, '.') 
+
+        self.agent_branches_flat = { f"{self.agent_name_id_map[agent_name]}.data.{key}": value for (key,value) in agent_flat.items() }
+
+        agent_block_id_name = { tokens[-2]: value for (key, value) in self.agent_branches_flat.items() if key.endswith('.name') and  'blocks' in key for tokens in [key.split('.')] } 
+        agent_block_id_type = { tokens[-2]: value for (key,value) in self.agent_branches_flat.items() if key.endswith('.type') and 'blocks' in key for tokens in [key.split('.')]}
+
+        # block parameters
+        agent_key_to_path   = { key: f"{ self.agent_id_name_map[ agentId ]}/{ agent_block_id_name[BlockId] if BlockId in agent_block_id_name else agent_block_id_type[BlockId]}/{'/'.join(key.split('.')[4:])}" 
+                                    for (key,value) in self.agent_branches_flat.items() if 'blocks' in key
+                                    for (agentId,BlockId) in [ (key.split('.')[0], key.split('.')[3] )]  }
+        # agent non-block parameters
+        agent_param_to_path = {  key: f"{ self.agent_id_name_map[ agentId[0]]}/{'/'.join(agentdata)}" 
+                                    for (key,value) in self.agent_branches_flat.items() if 'data' in key and 'blocks' not in key and '._' not in key and 'index' not in key
+                                    for (agentId,agentdata) in [ (key.split('.')[0:1], key.split('.')[1:]) ]}
+        # Combine them
+        agent_key_to_path  |= agent_param_to_path
+
+        path_to_agent_key   = { value: key for (key,value) in agent_key_to_path.items() }
+        return path_to_agent_key
+    
+    def listPaths(self):
+            return list(self.path_to_agent_key.keys())
+
+    def findBestMatch(self, search_for_path:str) -> str:
+            return max(self.path_to_agent_key.keys(), key=lambda path: fuzz.ratio(search_for_path, path ))
+
+    def findValueOf(self, path):
+            if path in self.path_to_agent_key:
+                    return self.agent_branches_flat[ self.path_to_agent_key[path] ]
+            else:
+                    return { "NotFoundPath": f"{path}", "Did you mean": max(self.path_to_agent_key.keys(), key=lambda aPath: fuzz.ratio( path, aPath )) } 
+
