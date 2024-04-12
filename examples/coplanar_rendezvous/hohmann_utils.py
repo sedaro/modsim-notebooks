69dc1f46f47d74ac7a81b5825165b1c01ab499f8
Paul Greene <paul.greene@sedarotech.com>
2024-04-12 16:32:01 -0400

    moved functions to utils

--- examples/coplanar_rendezvous/hohmann_utils.py
+++ examples/coplanar_rendezvous/hohmann_utils.py
@@ -4,6 +4,18 @@ Earth_radius_km = 6378.1363
 earth_u_ER = 1
 sec_per_TU = 806.811
 
+
+# helper functions
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+def angle_between(v1, v2):
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))  
+
+    
 # Circular Coplanar Phasing (different orbits)
 
 # Resources:
